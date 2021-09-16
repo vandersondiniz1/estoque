cc26530f1bf9ab010fcd029164015fd2f55afe21
Vanderson <vanderson.lima@fusiondms.com.br>
2021-09-16 09:01:23 -0300

    Implementação sistema de estoque

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,26 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
 
 class ProdutoController extends Controller
 {
     public function lista()
     {
         $produtos = DB::select('select * from produtos');
-        // return view('listagem')->with('produtos', $produtos);
-        return view('listagem')->withProdutos($produtos); //magic method
+        return view('produto.listagem')->withProdutos($produtos); //magic method
     }
 
     public function mostra()
     {
-        $id = 1;
+        $id = Request::input('id', '0');
         $resposta = DB::select('select * from produtos where id = ?', [$id]);
 
         if (empty($resposta)) {
             return "Esse produto não existe";
         }
-        return view('detalhes')->with('p', $resposta[0]);
+        return view('produto.detalhes')->with('p', $resposta[0]);
     }
 }
